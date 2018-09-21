bc21ad764f0817dcf34a4db707acb22b0eed6a58
kkeirstead3 <42755252+kkeirstead3@users.noreply.github.com>
2018-09-20 21:04:11 -0400

    Initial Commit

---
+++ ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  2340Project-iOS
+//
+//  Created by Kyle Keirstead on 9/20/18.
+//  Copyright © 2018 Kyle Keirstead. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
